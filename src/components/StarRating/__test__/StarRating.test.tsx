92ac6ae6c691db52177f8bd6858c5390a228b74d
fixruan <ruangoio01@gmail.com>
2022-09-23 16:59:23 -0300

    describe tests and coverage

--- src/components/StarRating/__test__/StarRating.test.tsx
+++ src/components/StarRating/__test__/StarRating.test.tsx
@@ -3,8 +3,22 @@ import { render } from '@testing-library/react-native';
 import { StarRating } from '../StarRating';
 
 describe('StarRating', () => {
-  test('the component rendered', () => {
-    const { debug } = render(<StarRating rating={{ average: 7 }} />);
-    debug();
+  describe('rating was passed', () => {
+    it('show the average', () => {
+      const { getByText } = render(<StarRating rating={{ average: 7 }} />);
+      expect(getByText('7')).toBeTruthy();
+    });
+
+    it('show the star icon', () => {
+      const { getByTestId } = render(<StarRating rating={{ average: 7 }} />);
+      expect(getByTestId('starIcon')).toBeTruthy();
+    });
+  });
+
+  describe('rating was NOT passing', () => {
+    it('return nothing', () => {
+      const { container } = render(<StarRating />);
+      expect(container.children.length).toEqual(0);
+    });
   });
 });
